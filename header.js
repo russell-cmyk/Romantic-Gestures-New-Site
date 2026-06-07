9387cea2efb72642cb2d8a3a024be53c9c3877cd
russell-cmyk <russell@creativestartupnz.com>
2026-06-07 15:26:35 +1200

    Update links for CONTACT and BLOG pages

--- header.js
+++ header.js
@@ -103,8 +103,8 @@ class SiteHeader extends HTMLElement {
                             <a href="/events/">Events</a>
                         </div>
                     </div>
-                    <a href="/contact/">CONTACT</a>
-                    <a href="/blog/">BLOG</a>
+                    <a href="contact.html">CONTACT</a>
+                    <a href="blog.html">BLOG</a>
                     <a href="/testimonials/">TESTIMONIALS</a>
                     <a href="/shop/" class="cart-icon" style="color: var(--brand-gold);">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
